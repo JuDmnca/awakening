6805a2ae876dde815e223c8fd444026768bfd316
Pierrot <p.pinto2oliveira@gmail.com>
2021-03-17 12:21:28 +0100

    Init question component 🎉

---
+++ front/components/World/Forms/Question.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>Hello c'est le form</h1>
+</template>
+
+<script>
+    export default {
+        name: 'question',
+        data() {
+            return {}
+        },
+        mounted() {
+        }
+    }
+</script>
+<style>
+
+</style>
+
