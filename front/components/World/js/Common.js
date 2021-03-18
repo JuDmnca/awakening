7b0841dbd5b1ba378fbe6aa52f2aa45e0c908bea
Julie De Mendonça <julie.demendonca@mmibordeaux.com>
2021-03-18 18:20:41 +0100

    :sparkles: GUI - Make it work

--- front/components/World/js/Common.js
+++ front/components/World/js/Common.js
@@ -3,6 +3,7 @@ import gsap from 'gsap'
 import Path from './Path'
 import Land from './Land'
 import Cube from './Cube'
+import ColorGUIHelper from './ColorGUIHelper'
 import { Group } from "three"
 
 class Common {
@@ -104,6 +105,14 @@ class Common {
            this.gui = require('three/examples/jsm/libs/dat.gui.module')
         }
         console.log('gui : ', this.gui)
+        const datGui  = new this.gui.GUI({ autoPlace: true });
+        datGui.domElement.id = 'gui'
+
+        const folder = datGui.addFolder(`Cube`)
+
+        folder.addColor(new ColorGUIHelper(this.myCube.cube.material,'color'),'value') //
+        .name('color')
+        // .onChange(animationLoop)
     }
 
     setSize() {
