b1a39415b788f9c0f4447feac2c74744af4b8b6e
Julie De Mendonça <julie.demendonca@mmibordeaux.com>
2021-03-23 12:52:12 +0100

    :art: Init Cube in Desert

--- front/components/World/js/Desert/Desert.js
+++ front/components/World/js/Desert/Desert.js
@@ -1,14 +1,22 @@
 import Land from '../Land'
+import Cube from './Cube'
 import modelDesert from '../../../../assets/models/desert_scaled.gltf'
+import MainGui from "../Helpers/MainGui"
 
 class Desert {
   constructor(props) {
     this.props = props
     this.land = new Land()
+    this.myCube = new Cube()
   }
 
   init(scene) {
     this.land.load(scene, modelDesert)
+
+    scene.add(this.myCube.cube)
+
+    // GUI
+    MainGui.init(this.myCube)
   }
 }
 
