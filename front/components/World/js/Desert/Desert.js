d1438f4e7bd4afbe809c8907ae59540b544114f1
Pierrot <p.pinto2oliveira@gmail.com>
2021-04-19 11:13:40 +0200

    ✨ Fog folder GUI

--- front/components/World/js/Desert/Desert.js
+++ front/components/World/js/Desert/Desert.js
@@ -110,6 +110,10 @@ export default class Desert {
     const lightsFolder = this.gui.gui.addFolder('Lights')
     lightsFolder.addColor(new ColorGUIHelper(this.spotLightOnFlowers, 'color'), 'value').name('flowers color')
     lightsFolder.add(this.spotLightOnFlowers, 'intensity', 0, 2, 0.1).name('intensity flowers')
+      // Fog and Background
+    const fogFolder = this.gui.gui.addFolder('Fog')
+    fogFolder.addColor(new ColorGUIHelper(scene.fog, 'color'), 'value').name('fog color') 
+    fogFolder.addColor(new ColorGUIHelper(scene, 'background'), 'value').name('background color') 
     
     // Listeners
     window.addEventListener('click', () => {
