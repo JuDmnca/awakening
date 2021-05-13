f11f7f4c82c799338770d0a84fdc999ec0e3e707
Pierrot <p.pinto2oliveira@gmail.com>
2021-05-13 14:50:14 +0200

    ✨ Add FPS indicator

--- front/components/World/js/Common.js
+++ front/components/World/js/Common.js
@@ -87,6 +87,8 @@ class Common {
     }
 
     init($canvas) {
+        (function(){var script=document.createElement('script');script.onload=function(){var stats=new Stats();document.body.appendChild(stats.dom);requestAnimationFrame(function loop(){stats.update();requestAnimationFrame(loop)});};script.src='//mrdoob.github.io/stats.js/build/stats.min.js';document.head.appendChild(script);})()
+
         this.setSize()
 
         this.scene = new THREE.Scene()
