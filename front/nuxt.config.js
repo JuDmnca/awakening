1951e512eb457b55721b5857c60552e5fecc0d6d
PINTO DE OLIVEIRA Pierre <ppintodeoliveira@g-a102a-10.local>
2021-05-18 17:40:11 +0200

    :construction: Have to add bloom pass

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -72,7 +72,7 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    transpile: ['three/examples/jsm/controls/OrbitControls', 'three/examples/jsm/loaders/GLTFLoader'],
+    transpile: ['three/examples/jsm/controls/OrbitControls', 'three/examples/jsm/loaders/GLTFLoader', 'three/examples/jsm/postprocessing/EffectComposer.js','three/examples/jsm/postprocessing/RenderPass.js', 'three/examples/jsm/postprocessing/UnrealBloomPass.js'],
     extend (config, ctx) {
       if (!!config.module) {
         config.module.rules.push({ test: /\.(vert|frag)$/i, use: ["raw-loader"] });
