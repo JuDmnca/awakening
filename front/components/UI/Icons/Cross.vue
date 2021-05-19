9c1cf5c966397a8ce0294b36c24a8cc32b46f9bf
PINTO DE OLIVEIRA Pierre <ppintodeoliveira@g-a102a-10.local>
2021-05-19 12:20:43 +0200

    :sparkles: Add display and close on click

--- front/components/UI/Icons/Cross.vue
+++ front/components/UI/Icons/Cross.vue
@@ -1,5 +1,5 @@
 <template>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14" :aria-labelledby="iconName" :width="width" :height="height" >
+    <svg ref="cross" class="cross" viewBox="0 0 14 14" :aria-labelledby="iconName" :width="width" :height="height" >
         <title :id="iconName"> {{iconName}} </title>
         <g :fill="iconColor">
             <path id="Tracé_126" data-name="Tracé 126" d="M19,6.41,17.59,5,12,10.59,6.41,5,5,6.41,10.59,12,5,17.59,6.41,19,12,13.41,17.59,19,19,17.59,13.41,12Z" transform="translate(-5 -5)" />
@@ -24,7 +24,31 @@
             iconColor: {
                 type: String,
                 default: 'white'
+            },
+            toClose: {
+                type: Boolean,
+                default: false
             }
+        },
+        mounted() {
+            if(this.toClose) {
+                this.$refs.cross.addEventListener('click', () => {
+                    this.$store.commit('constellation/toggleVisible')
+                })
+            }
+        },
+        updated() {
         }
     }
 </script>
+
+<style scoped>
+    .cross {
+        padding: 20px;
+    }
+
+    .cross:hover {
+        cursor: pointer;
+    }
+
+</style>
