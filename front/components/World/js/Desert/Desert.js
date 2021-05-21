d99e7edf86634730bf0ae9f2b8dfceb75f0994b4
Pierrot <p.pinto2oliveira@gmail.com>
2021-05-21 16:31:43 +0200

    🐛 Fix color issue to skyboxes

--- front/components/World/js/Desert/Desert.js
+++ front/components/World/js/Desert/Desert.js
@@ -189,7 +189,7 @@ export default class Desert {
     this.spores.particles.add(this.spotLightOnFlowers)
 
     // Fog
-    const colorBG = new THREE.Color('#404040')
+    const colorBG = new THREE.Color('#040408')
     scene.fog = new THREE.Fog(colorBG, 10, 300)
     const loader = new THREE.CubeTextureLoader()
     loader.premultiplyAlpha = true
@@ -216,7 +216,7 @@ export default class Desert {
     )
 
     skybox.material.uniforms.envMap.value = texture
-
+    skybox.layers.enable(1)
     Object.defineProperty(skybox.material, 'envMap', {
 
       get () {
