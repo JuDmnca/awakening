8ee294c1069488726e92e27f3de7b2e70f65b64e
Pierrot <p.pinto2oliveira@gmail.com>
2021-06-03 12:12:11 +0200

    ✨ Re organisation of ui profile

--- front/components/Constellation/Profile.vue
+++ front/components/Constellation/Profile.vue
@@ -1,28 +1,30 @@
 <template>
-  <section style="color: white;" class="profile flex flex-col items-center">
-    <img ref="userImg" class="profile__img" src="">
-    <h2 class="profile__name">
-      <span>{{ $store.state.constellation.currentUser.name }} </span>
-    </h2>
-    <p class="profile__description small">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ultrices quam sed neque placerat, non laoreet magna viverra. Sed maximus eros non dui cursus condimentum. Vestibulum libero nisl, feugiat quis magna id, iaculis pretium ante. Duis sem felis, accumsan ut quam et, tincidunt sagittis odio.
-    </p>
-    <div class="profile__responses flex flex-col w-1/2">
-      <div class="responses flex flex-col">
-        <p class="small">
-          Votre odeur
-        </p>
-        <h2 class="responses__title">
-          {{ $store.state.constellation.currentUser.smell }}
-        </h2>
-      </div>
-      <div class="responses flex flex-col">
-        <p class="small">
-          Votre son
-        </p>
-        <h2 class="responses__title">
-          Les klaxons
-        </h2>
+  <section style="color: white;" class="profile flex">
+    <div class="profile__left flex flex-col items-center">
+      <img ref="userImg" class="profile__img" src="">
+      <h2 class="profile__name">
+        <span>{{ $store.state.constellation.currentUser.name }} </span>
+      </h2>
+      <p class="profile__description small">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ultrices quam sed neque placerat, non laoreet magna viverra. Sed maximus eros non dui cursus condimentum. Vestibulum libero nisl, feugiat quis magna id, iaculis pretium ante. Duis sem felis, accumsan ut quam et, tincidunt sagittis odio.
+      </p>
+      <div class="profile__responses flex flex-col w-1/2">
+        <div class="responses flex flex-col">
+          <p class="small">
+            Votre odeur
+          </p>
+          <h2 class="responses__title">
+            {{ $store.state.constellation.currentUser.smell }}
+          </h2>
+        </div>
+        <div class="responses flex flex-col">
+          <p class="small">
+            Votre son
+          </p>
+          <h2 class="responses__title">
+            Les klaxons
+          </h2>
+        </div>
       </div>
     </div>
     <!-- <h3 ref="previous">
@@ -42,12 +44,23 @@
       </button>
       <span v-if="errorMessage" style="color:red">{{ errorMessage }}</span>
     </form> -->
-    <div class="absolute border border-white rounded-full close p-6" @click.prevent="$nuxt.$emit('onCrystalClick')">
-      <UI-Icons-Cross
-        :width="16"
-        :height="16"
-        :color="'#FFF'"
-      />
+    <div class="profile__right flex flex-col">
+      <div class="profile__icon profile__icon--close border border-white rounded-full flex" @click.prevent="$nuxt.$emit('onCrystalClick')">
+        <UI-Icons-Cross
+          class="m-auto"
+          :width="16"
+          :height="16"
+          :color="'#FFF'"
+        />
+      </div>
+      <div class="profile__icon profile__icon--random border border-white rounded-full flex self-center" @click.prevent="getRandomProfile()">
+        <UI-Icons-Dice
+          class="m-auto"
+          :color="'#FFF'"
+          :width="25"
+          :height="25"
+        />
+      </div>
     </div>
   </section>
 </template>
@@ -133,7 +146,7 @@ export default {
   font-weight: 300;
   position: relative;
   z-index: 10;
-  padding: 0 100px 100px 100px;
+  padding: 100px 100px 100px 100px;
   font-size: 44px;
 }
 
@@ -167,13 +180,21 @@ export default {
 }
 
 .profile__img {
-  width: 10%;
-  margin-top: 100px;
+  height: 10vh;
 }
 
-.close {
-  top: 100px;
-  right: 100px;
+.profile__icon {
   cursor: pointer;
+}
+
+.profile__icon--close {
+  width: 75px;
+  height: 75px;
+  margin-bottom: 50px;
+}
+
+.profile__icon--random {
+  width: 60px;
+  height: 60px;
 }
 </style>
